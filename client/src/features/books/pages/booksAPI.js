c054dccdff5b1581cacb5b62076c792676f9c330
Alexander John <96aljohn@gmail.com>
2025-03-22 22:18:58 -0700

    refactor: improve formatting and indentation in InputField component and booksAPI function

--- client/src/features/books/pages/booksAPI.js
+++ client/src/features/books/pages/booksAPI.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const BASE_URL = "http://localhost:8800";
 
 export const createBook = (bookData) => {
-  return axios.post(`${BASE_URL}/books`, bookData);
+    return axios.post(`${BASE_URL}/books`, bookData);
 };
